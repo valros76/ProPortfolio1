0c34b290f530c38257a5aa2e33a1d817be8b534f
valros76 <valerian.dufrene@gmail.com>
2019-01-01 18:32:02 +0100

    Add base files

--- views/homeview.php
+++ views/homeview.php
@@ -4,27 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/sketchy/bootstrap.min.css"/>
     <link rel="stylesheet" href="public/css/style.css"/>
     <title>Home</title>
 </head>
 <body>
 
-    <header class="container-fluid">
-        <h1 class="col-sm">Accueil</h1>
-        <nav class="container-fluid headMenu">
-            <ul class="row">
-                <li class="col-md"><a href="?action=home">Home</a></li>
-                <li class="col-md"><a href="?action=portfolio">Portfolio</a></li>
-                <li class="col-md"><a href="?action=contact">Contact</a></li>
+    <header>
+        <h1>Accueil</h1>
+        <nav class="headMenu">
+            <ul>
+                <li><a href="?action=home">Home</a></li>
+                <li><a href="?action=portfolio">Portfolio</a></li>
+                <li><a href="?action=contact">Contact</a></li>
             </ul>
         </nav>
     </header>
 
-    <section class="container-fluid">
-        <article class="row">
-            <h2 class="col-md-auto">Article $</h2>
-            <p class="col-md">Contenu de l'article $</p>
+    <section>
+        <article>
+            <h2>Article $</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quod in ullam odio ea blanditiis fuga temporibus alias eum earum! Vero porro cum pariatur cumque explicabo minus ipsum ullam eaque. Quod, animi quo rem molestiae fuga officia, quaerat nobis voluptas quos cumque minus laboriosam dolore distinctio soluta temporibus consectetur est quasi culpa, nesciunt nostrum. Sunt iste id, voluptatum repellendus quidem vitae dolorum ullam velit nostrum architecto soluta distinctio, ducimus eum, eligendi quasi totam saepe consequatur qui adipisci corrupti esse animi?</p>
         </article>
     </section>
     
