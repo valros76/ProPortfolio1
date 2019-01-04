95adbded1b91246deadf3d7e53a403e570f87a7e
valros76 <valerian.dufrene@gmail.com>
2019-01-04 07:56:21 +0100

    Add cv.php page.

---
+++ views/pages/cv.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/css/style.css"/>
+    <title>CV</title>
+</head>
+<body>
+
+    <header>
+        <h1>Mon CV</h1>
+        <nav class="headMenu">
+            <ul>
+                <li><a href="?action=home">Home</a></li>
+                <li><a href="?action=portfolios">Mes Portfolios</a></li>
+                <li><a href="?action=contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <article class="cv">
+            <h2 class="cvTitle">CV</h2>
+            <p class="cvContent">
+                Valérian Dufrène<br/>
+                41 rue des pinsons, 76690 Clères<br/>
+                <a href="tel:+33750474757" title="Appeler le numéro">0750474757</a><br/>
+                Diplôme : Développeur intégrateur en réalisation d'applications web.<br/>
+                Je travaille exclusivement en remote.<br/><br/>
+                Formation :<br/>
+                Parcours de formation Développeur Web Junior chez Openclassrooms.<br/>
+            </p>
+        </article>
+    </section>
+    
+    <footer>
+        <p>
+            Créé par Valérian Dufrène - 2018
+        </p>
+    </footer>
+
+</body>
+</html>
