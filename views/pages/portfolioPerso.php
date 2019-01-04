7549be94fa3343f3157ce4b1a1d1bca65455e7be
valros76 <valerian.dufrene@gmail.com>
2019-01-04 07:36:48 +0100

    Add new project in personnal portfolio with gameZip.

--- views/pages/portfolioPerso.php
+++ views/pages/portfolioPerso.php
@@ -22,13 +22,25 @@
 
     <section>
         <article class="portfolioProArt">
-            <h2>Projet 1 - Webagency</h2>
+            <h2>Projet personnel 1 - Shifumi</h2>
             <p>
                 Projet personnel : développer un mini-jeu de Shifumi (Pierre, Feuille, Ciseaux).<br/>
                 <code>Langages : HTML / CSS / JS</code><br/>
                 <code>CMS : Non</code><br/>
                 <a href="?action=shifumi">> Jouer au Shifumi <</a>
                 <img src="assets/images/shifumi01.png" alt="Screen du projet shifumi"/>
+            </p>
+        </article>
+        <article class="portfolioProArt">
+            <h2>Projet personnel 2 - Tetris</h2>
+            <p>
+                Projet personnel : développer un Tetris en LUA.<br/>
+                Pour jouer au jeu, téléchargez le zip.<br/>
+                Ensuite, lancez le fichier Tetralros.exe.<br/>
+                Vous pouvez vous diriger avec les flèches directionnelles, la flèche du haut servant à tourner les pièces.<br/>
+                <code>Langages : LUA</code><br/>
+                <code>CMS : Non</code><br/>
+                <a href="assets\download\Tetralros - Distribution privée.zip" download="Tetris_by_valerian_dufrene">> Télécharger le jeu <</a>
             </p>
         </article>
     </section>
